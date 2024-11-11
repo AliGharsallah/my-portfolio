ff9ef9f8648cb504d85e624accb26b583d66e845
Ali gh <ali.gharsaallah@gmail.com>
2024-11-11 16:04:29 +0100

    Ajout de configuration de normalisation des fins de ligne

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Projects.css"
 
-const  Projects=()=>{
-return(
-    <div className="projects-container">
-    <hr className="hr-projects"/>
-    <span className="project-title">Projects</span>
-    </div>
-)
+const Projects = () => {
+    return (
+        <div className="projects-container">
+            <hr className="hr-projects" />
+            <span className="project-title">Projects</span>
+            <p>Explore My Work: A Showcase of Creativity and Innovation</p>
+            <div className="projects">
+                   
+                <div>
+                    <span>student manager</span>
+                    <button></button>
+
+                </div>
+                <div><span>E-commerce</span>
+                    <button></button>
+                </div>
+                <div><span>IntelliHire</span>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    )
 
 
 }
-export  default Projects; 
+export default Projects; 
